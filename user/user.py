337d12c7915ff35a743f855525ef698d3f7de218
Daniel Santos <dsantosp12@gmail.com>
2015-09-03 17:01:37 -0400

    create user controller created

--- user/user.py
+++ user/user.py
@@ -1,2 +1,31 @@
-from models import User
+import datetime
 
+from models import User, db
+
+
+def create_user(**kwargs):
+    """Create user control."""
+    db.connect()
+    User.create(
+        first_name=kwargs['first_name'],
+        second_name=kwargs['second_name'],
+        username=kwargs['username'],
+        password=kwargs['password'],
+        joined=datetime.datetime.now,
+        university_name=kwargs['university_name'],
+        university_email=kwargs['university_email'],
+        personal_email=kwargs['personal_email'],
+    )
+    db.close()
+
+
+if __name__ == '__main__':
+    create_user(
+        first_name='Daniel',
+        second_name='Santos',
+        username='dsantos',
+        password='test',
+        university_name='UML',
+        university_email='daniel_santos@student.uml.edu',
+        personal_email='dsantosp12@gmail.com'
+    )
