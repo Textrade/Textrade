44bbf2ea172f1ca47dce6088733b7ffa3ea10c7f
Daniel Santos <dsantosp12@gmail.com>
2015-09-03 19:27:15 -0400

    part of the front-end, merged to the back-end

--- Textrade.py
+++ Textrade.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, g, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('default/index.html')
+
+
+@app.route('/team')
+def team():
+    return render_template('misc/the-team.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('user/login.html')
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
